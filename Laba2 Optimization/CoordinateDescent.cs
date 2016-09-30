2a82272c0ed49db3c63279b1e615455f5afa60b6
Dima Lupyak <dima.lupyak@gmail.com>
2016-09-30 12:11:42 +0300

    Added Coordinate Descent method

---
+++ Laba2 Optimization/CoordinateDescent.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimization
+{
+    public static class CoordinateDescent
+    {
+        public static void GetMin(Func<double, double, double> f, double eps, out double x1, out double x2)
+        {
+            double h = 50, alpha = 2;
+            x1 = 1;
+            x2 = 1;
+            Console.WriteLine(new String('-', 52));
+            Console.WriteLine("{0,35}{1,18}", "Coordinate Descent", "|");
+            Console.WriteLine(new String('-', 52) + "|");
+            Console.WriteLine(
+                            "{0,3} |{1,10} |{2,10} |{3,10} |{4,10} |",
+                            "k", "x1", "x2", "h", "y");
+            Console.WriteLine(
+                   "{0}{1}{1}{1}{1}",
+                   "----|", "-----------|");
+            for (int k = 0; h > eps; k++ )
+            {
+                int direction; int i =0;
+                do
+                {
+                    if (f(x1, x2) > f(x1 + h, x2))
+                    {
+                        direction = 1;
+                    }
+                    else if (f(x1, x2) > f(x1 - h, x2))
+                    {
+                        direction = -1;
+                    }
+                    else direction = 0;
+                    if (direction != 0)
+                    {
+                        x1 += direction * h;
+                        i++;
+                    }
+                }
+                while (direction != 0 && i < 100);
+                i = 0;
+                do
+                {
+                    if (f(x1, x2) > f(x1, x2 + h))
+                    {
+                        direction = 1;
+                    }
+                    else if (f(x1, x2) > f(x1, x2 - h))
+                    {
+                        direction = -1;
+                    }
+                    else direction = 0;
+                    if (direction != 0)
+                    {
+                        x2 += direction * h;
+                        i++;
+                    }
+                }
+                while (direction != 0 && i < 100);
+                Console.WriteLine(
+                            "{0,3} |{1,10:0.0000} |{2,10:0.0000} |{3,10:0.0000} |{4,10:0.0000} |",
+                            k, x1, x2, h, f(x1,x2));
+                h /= alpha;
+            }
+            Console.WriteLine(new String('-', 52));
+        }
+
+        public static void GetMax(Func<double, double, double> f, double eps, out double x1, out double x2)
+        {
+            GetMin((a, b) => -f(a, b), eps, out x1, out x2);
+        }
+    }
+}
---
+++ Laba2 Optimization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimization
+{
+    class Program
+    {
+        static double f(double x1, double x2)
+        {
+            return x1*x1+x2*x2-x1*x2;
+        }
+
+        static void Main(string[] args)
+        {
+            double x1, x2;
+            CoordinateDescent.GetMin(f, 0.0001, out x1, out x2);
+            double y = f(x1, x2);
+            Console.WriteLine("x1 = {0:0.000} \t x2 = {1:0.000} \t y(x) = {2:0.000}\n", x1, x2, y);
+            Console.ReadKey();
+        }
+    }
+}
