e5613742a4aeec9c040de4f9775421118ab14d00
Dima Lupyak <dima.lupyak@gmail.com>
2016-10-05 22:26:07 +0300

    Added iteration displaying

--- Laba2 Optimization/DownhillSimplex.cs
+++ Laba2 Optimization/DownhillSimplex.cs
@@ -10,8 +10,18 @@ namespace Optimization
         static double Betta = 0.5;
         static double Gamma = 2;
         public static void GetMin(Func<double, double, double> f, double eps, out double x1result, out double x2result)
-        {  
+        {
+            Console.WriteLine(new String('-', 72));
+            Console.WriteLine("{0,45}{1,28}", "Downhill Simplex", "|");
+            Console.WriteLine(new String('-', 72) + "|");
+            Console.WriteLine(
+                            "{0,3} |{1,15} |{2,15} |{3,15} |{4,15} |",
+                            "k", "xl", "xg", "xh", "yc");
+            Console.WriteLine(
+                   "{0}{1}{1}{1}{1}",
+                   "----|", "----------------|");
             #region Ініціалізація
+            int k = 0;
             X xh, xg, xl, xc, xr;
             X x1 = new X(-10, 10, f);
             X x2 = new X(10, 10, f);
@@ -23,19 +33,20 @@ namespace Optimization
 
             #region 1 Сортування
         region1:
+            k++;
             List<X> x = new List<X> { xl, xg, xh };
             x.Sort();
             xl = x[0];
             xg = x[1];
             xh = x[2];
-            #endregion     
-       
+            #endregion
+
             #region 2 Центр ваг
             xc = (xg + xl) / 2;
             #endregion
 
             #region 3 Відображення
-            xr = (1 + Alpha) * xc - Alpha * xh; 
+            xr = (1 + Alpha) * xc - Alpha * xh;
             #endregion
 
             #region 4 Розтягування
@@ -48,25 +59,26 @@ namespace Optimization
                 goto region8;
             }
             //4b
-            else if(xl.F < xr.F && xr.F < xg.F)
+            else if (xl.F < xr.F && xr.F < xg.F)
             {
                 xh = xr;
                 goto region8;
             }
             //4c
-            else if(xh.F > xr.F && xr.F > xg.F)
+            else if (xh.F > xr.F && xr.F > xg.F)
             {
                 X tmp = xr;
                 xr = xh;
-                xh = tmp;                
+                xh = tmp;
             }
             #endregion
 
             #region 5 Стиснення
             X xs = Betta * xh + (1 - Betta) * xc;
             #endregion
+
             #region 6
-            if(xs.F < xh.F)
+            if (xs.F < xh.F)
             {
                 X tmp = xs;
                 xs = xh;
@@ -74,17 +86,21 @@ namespace Optimization
                 goto region8;
             }
             #endregion
+
             #region 7 Стиснення симплексу
-            else 
+            else
             {
-                xg = xl + (xg-xl)/2;
-                xh = xl + (xh-xl)/2;
+                xg = xl + (xg - xl) / 2;
+                xh = xl + (xh - xl) / 2;
             }
             #endregion
-        
+
             #region 8
         region8:
-            double Sigma = Math.Sqrt((Math.Pow(xl.F - xc.F, 2) + Math.Pow(xg.F - xc.F, 2) + Math.Pow(xh.F - xc.F, 2))/4);
+            double Sigma = Math.Sqrt((Math.Pow(xl.F - xc.F, 2) + Math.Pow(xg.F - xc.F, 2) + Math.Pow(xh.F - xc.F, 2)) / 4);
+            Console.WriteLine(
+                            "{0,3} |{1,15:0.0000} |{2,15:0.0000} |{3,15:0.0000} |{4,15:0.0000} |",
+                            k, xl, xg, xh, xc.F);
             if (Sigma > eps)
             {
                 goto region1;
@@ -92,6 +108,7 @@ namespace Optimization
             #endregion
             x1result = xc.X1;
             x2result = xc.X2;
+            Console.WriteLine(new String('-', 72));
         }
 
         public static void GetMax(Func<double, double, double> f, double eps, out double x1, out double x2)
@@ -130,6 +147,10 @@ namespace Optimization
             public int CompareTo(X other)
             {
                 return F.CompareTo(other.F);
+            }
+            override public string ToString()
+            {
+                return String.Format("({0:0.00};{1:0.00})", X1, X2);
             }
         }
     }
