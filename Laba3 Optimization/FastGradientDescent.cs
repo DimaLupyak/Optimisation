bd310d1daf27841968e8535f5cd911803618c059
Dima Lupyak <dima.lupyak@gmail.com>
2016-10-20 13:34:26 +0300

    Eddited Fast Gradient Descent Method

--- Laba3 Optimization/FastGradientDescent.cs
+++ Laba3 Optimization/FastGradientDescent.cs
@@ -8,13 +8,9 @@ namespace Laba3_Optimization
 {
     class FastGradientDescent
     {
-        public static X GetMin(Function f, X x0, double Lambda, double eps)
+        public static X GetMin(Function f, X x, double Lambda, double eps)
         {
-            X x = x0;
-            X temp;
             int k = 0;
-            X xp = x;
-
             Console.WriteLine(new String('-', 84));
             Console.WriteLine("{0,55}{1,30}", "Fast Gradient Descent", "|");
             Console.WriteLine(new String('-', 84) + "|");
@@ -24,27 +20,17 @@ namespace Laba3_Optimization
             Console.WriteLine(
                    "{0}{2}{1}{1}{2}{1}",
                    "----|", "-----------|", "---------------------|");
-            while (f.Norma(xp) > eps)
+            while (f.Norma(x) > eps)
             {
                 Console.WriteLine(
                             "{0,3} |{1,20} |{2,10:0.0000} |{3,10:0.00} |{4,20} |{5,10:0.000} |",
-                            k, x, f.F(x), Lambda, new X(f.Fdx1(xp), f.Fdx2(xp)), f.Norma(xp));
-
-                temp = new X(x.X1 - Lambda * f.Fdx1(xp), x.X2 - Lambda * f.Fdx2(xp));
-                if (f.F(temp) < f.F(x))
-                {
-                    k++;
-                    x = temp;
-                    continue;
-                }
-                else
-                {
-                    Lambda /= 1.2;
-                    xp = x;
-                }
+                            k, x, f.F(x), Lambda, new X(f.Fdx1(x), f.Fdx2(x)), f.Norma(x));
+                Lambda = Dichotomy.GetMin((lambda) => f.F(new X(x.X1 - lambda * f.Fdx1(x), x.X2 - lambda * f.Fdx2(x))), -100, 100, 0.1);
+                x = new X(x.X1 - Lambda * f.Fdx1(x), x.X2 - Lambda * f.Fdx2(x));
+                k++;                
             }
             Console.WriteLine(new String('-', 84));
-            return xp;
+            return x;
         }
     }
 }
